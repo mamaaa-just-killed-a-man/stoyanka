ecddfa3ccb802ffa3e05b954dc584ebc32544dd8
sofikmorozova <131594726+sofikmorozova@users.noreply.github.com>
2023-04-25 02:52:18 +0300

    Update layout.php

--- stoyanka/layout.php
+++ stoyanka/layout.php
@@ -3,11 +3,11 @@
   <head>
     <meta charset="utf-8">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto:ital,wght@0,700;1,400&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto:ital,wght@0,700;1,400&display=swap" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet"> 
     <title>Сеть отелей Стоянка</title>
-</head>
+  </head>
 <body>
   <div class="container">
     <?php require('header.php'); ?>
@@ -23,9 +23,51 @@
 <div class="hello-img"><img src = "img/beau.jpg"  width = "902" height = "823" alt = "Фотография отеля"></div>
 </section>
 
+    <h1 class="o-nas">О НАС</h1>
+<p class="info">
+  Мало на что в этом мире можно положиться – 
+  <br>явно не наш лозунг. Нашим остановкам
+  <br>доверяют миллионы. STOYANKA берет свое
+  <br>начало в 2017, и за такой, казалось бы, недолгий
+  <br>промежуток времени, уже успела завоевать
+  <br>народное признание и хорошую репутацию.
+  <br>Наши отели отличаются своей доступностью и
+  <br>комфортом, абсолютно в каждой точке Вам будут
+  <br>доступны Wi-Fi, тренажерный зал, крытый
+  <br>бассейн, круглосуточный шведский стол, а также
+  <br>парковочное место. Звучит роскошно, не так ли?
+</p>
+<ul class="hotel-pictures">
+  <li class="pic">
+    <img class="image" src="https://sun9-7.userapi.com/impg/JM-DayLxff0HcFcq29Q4_EyISY6aoAmMSDkv7A/k25t5u28gVM.jpg?size=550x637&quality=96&sign=db100ec1e3ea0ce58e724aa0d44a6ade&type=album">
+  </li>
+  <li class="pic">
+    <img class="image" src="https://sun9-42.userapi.com/impg/eszAfNt_pmp4DAaDpYCHsFs_CXxTXZ3ESFzQiA/RweGgjitf8U.jpg?size=564x846&quality=96&sign=68e74aa7864e5497608cec30f3fa6309&type=album">
+  </li>
+  <li class="pic">
+    <img class="image" src="https://sun9-16.userapi.com/impg/hRop6ef1I49Z45neB_uvGRWuS-2nSXYwIAqifg/FVcBprEi9WA.jpg?size=564x826&quality=96&sign=f662da003ad11f46060ef09cbead51d9&type=album">
+  </li>
+</ul>
+<h2 class="nomera">НОМЕРА</h2>
+<ul class="room-pictures">
+  <li class="pic">
+    <img class="image" src="https://sun9-71.userapi.com/impg/iOYu2ZDepv5htoRwVed3lG6nQB1gMO5yF2z8kQ/Asq4XuPhmdE.jpg?size=563x751&quality=95&sign=642212da7e12927870c03cd0ad9acacf&type=album" alt="Стандарт" width="120" height="120">
+    <h3 class="title">Стандарт</h3>
+  </li>
+  <li class="pic">
+    <img class="image" src="https://sun9-1.userapi.com/impg/LRFKdjDEsokkyx4AY02na9OLbbo1fJtkjpoQBg/Z0B6fZRQhlM.jpg?size=564x846&quality=95&sign=d2723a1ae50fe1173eefeab24fb58ce9&type=album" alt="Люкс" width="120" height="120">
+    <h3 class="title">Люкс</h3>
+  </li>
+  <li class="pic">
+    <img class="image" src="https://sun9-29.userapi.com/impg/H_OfHOAjV6hEs0zVwYRLUNoiOmXIlcUOmzi47A/K7f9u6XeRIc.jpg?size=563x469&quality=95&sign=3e731f2f82175bddb69e58c6945b64f1&type=album" alt="Семейный" width="120" height="120">
+    <h3 class="title">Семейный</h3>
+  </li>
+</ul>
 
-
-  </main>
+    
+    </main>    
+    
+    
   <footer>
     <div class="main-footer"><p>Контакты</p>
         <ul class = "contacts">
