a2ff3ddabc1b3b682540cfdf70dc7dfd30083eb0
sofikmorozova <131594726+sofikmorozova@users.noreply.github.com>
2023-05-28 17:33:09 +0000

    Update layout.php

--- stoyanka/layout.php
+++ stoyanka/layout.php
@@ -1,77 +1,228 @@
 <!DOCTYPE html>
 <html lang="ru">
-  <head>
+<head>
     <meta charset="utf-8">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto:ital,wght@0,700;1,400&display=swap" rel="stylesheet">
-    <link href="css/header.css" rel="stylesheet">
-<link href="css/style.css" rel="stylesheet"> 
-<link href="css/footer.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto:ital,wght@0,700;1,400&display=swap"
+          rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
     <title>Сеть отелей Стоянка</title>
 </head>
 <body>
-  <div class="container">
+<div class="container">
     <?php require('header.php'); ?>
-  <main class="container">
-  <section class= "hello"><div class="hello-text"><p class= "opening">Сеть бюджетных отелей по всему миру</p>
-    <h1>Остановки, которым <br> <em>доверяют миллионы</em></h1>
-<div class= "registration-text"><p>Уютные отели, расположенные в самом центре городов. В вашем распоряжении все необходимые удобства. О таком отдыхе не стыдно рассказать друзьям и показать в Instagram.
-    </p>
-    <form><button class = "join"><a href = "sign-up.php">Присоединяйтесь!</a></button></form>
-  </div>
+    <main class="container">
+        <section class="hello">
+            <div class="hello-text"><p class="opening">Сеть бюджетных отелей по всему миру</p>
+                <h1>Остановки, которым <br> <em>доверяют миллионы</em></h1>
+                <div class="registration-text"><p>Уютные отели, расположенные в самом центре городов. В вашем
+                        распоряжении все необходимые удобства. О таком отдыхе не стыдно рассказать друзьям и показать в
+                        Instagram.
+                    </p>
+                    <p><input type="button" value="Присоединяйтесь!"></p>
+                </div>
 
-</div>
-<div class="hello-img"><img src = "img/beau.jpg"  width = "902" height = "823" alt = "Фотография отеля"></div>
-</section>
+            </div>
+            <div class="hello-img"><img src="img/beau.jpg" width="902" height="823" alt="Фотография отеля"></div>
+        </section>
 
-    <h1 class="o-nas">О НАС</h1>
-<p class="info">
-  Мало на что в этом мире можно положиться – 
-  <br>явно не наш лозунг. Нашим остановкам
-  <br>доверяют миллионы. STOYANKA берет свое
-  <br>начало в 2017, и за такой, казалось бы, недолгий
-  <br>промежуток времени, уже успела завоевать
-  <br>народное признание и хорошую репутацию.
-  <br>Наши отели отличаются своей доступностью и
-  <br>комфортом, абсолютно в каждой точке Вам будут
-  <br>доступны Wi-Fi, тренажерный зал, крытый
-  <br>бассейн, круглосуточный шведский стол, а также
-  <br>парковочное место. Звучит роскошно, не так ли?
-</p>
-<ul class="hotel-pictures">
-  <li class="pic">
-    <img class="image" src="https://sun9-7.userapi.com/impg/JM-DayLxff0HcFcq29Q4_EyISY6aoAmMSDkv7A/k25t5u28gVM.jpg?size=550x637&quality=96&sign=db100ec1e3ea0ce58e724aa0d44a6ade&type=album">
-  </li>
-  <li class="pic">
-    <img class="image" src="https://sun9-42.userapi.com/impg/eszAfNt_pmp4DAaDpYCHsFs_CXxTXZ3ESFzQiA/RweGgjitf8U.jpg?size=564x846&quality=96&sign=68e74aa7864e5497608cec30f3fa6309&type=album">
-  </li>
-  <li class="pic">
-    <img class="image" src="https://sun9-16.userapi.com/impg/hRop6ef1I49Z45neB_uvGRWuS-2nSXYwIAqifg/FVcBprEi9WA.jpg?size=564x826&quality=96&sign=f662da003ad11f46060ef09cbead51d9&type=album">
-  </li>
-</ul>
-<h2 class="nomera">НОМЕРА</h2>
-<ul class="room-pictures">
-  <li class="pic">
-    <img class="image" src="https://sun9-71.userapi.com/impg/iOYu2ZDepv5htoRwVed3lG6nQB1gMO5yF2z8kQ/Asq4XuPhmdE.jpg?size=563x751&quality=95&sign=642212da7e12927870c03cd0ad9acacf&type=album" alt="Стандарт" width="120" height="120">
-    <h3 class="title">Стандарт</h3>
-  </li>
-  <li class="pic">
-    <img class="image" src="https://sun9-1.userapi.com/impg/LRFKdjDEsokkyx4AY02na9OLbbo1fJtkjpoQBg/Z0B6fZRQhlM.jpg?size=564x846&quality=95&sign=d2723a1ae50fe1173eefeab24fb58ce9&type=album" alt="Люкс" width="120" height="120">
-    <h3 class="title">Люкс</h3>
-  </li>
-  <li class="pic">
-    <img class="image" src="https://sun9-29.userapi.com/impg/H_OfHOAjV6hEs0zVwYRLUNoiOmXIlcUOmzi47A/K7f9u6XeRIc.jpg?size=563x469&quality=95&sign=3e731f2f82175bddb69e58c6945b64f1&type=album" alt="Семейный" width="120" height="120">
-    <h3 class="title">Семейный</h3>
-  </li>
-</ul>
+        <h1 class="o-nas">О НАС</h1>
+        <p class="info">
+            Мало на что в этом мире можно положиться –
+            <br>явно не наш лозунг. Нашим остановкам
+            <br>доверяют миллионы. STOYANKA берет свое
+            <br>начало в 2017, и за такой, казалось бы, недолгий
+            <br>промежуток времени, уже успела завоевать
+            <br>народное признание и хорошую репутацию.
+            <br>Наши отели отличаются своей доступностью и
+            <br>комфортом, абсолютно в каждой точке Вам будут
+            <br>доступны Wi-Fi, тренажерный зал, крытый
+            <br>бассейн, круглосуточный шведский стол, а также
+            <br>парковочное место. Звучит роскошно, не так ли?
+        </p>
+        <ul class="hotel-pictures">
+            <li class="pic">
+                <img class="image"
+                     src="https://sun9-7.userapi.com/impg/JM-DayLxff0HcFcq29Q4_EyISY6aoAmMSDkv7A/k25t5u28gVM.jpg?size=550x637&quality=96&sign=db100ec1e3ea0ce58e724aa0d44a6ade&type=album">
+            </li>
+            <li class="pic">
+                <img class="image"
+                     src="https://sun9-42.userapi.com/impg/eszAfNt_pmp4DAaDpYCHsFs_CXxTXZ3ESFzQiA/RweGgjitf8U.jpg?size=564x846&quality=96&sign=68e74aa7864e5497608cec30f3fa6309&type=album">
+            </li>
+            <li class="pic">
+                <img class="image"
+                     src="https://sun9-16.userapi.com/impg/hRop6ef1I49Z45neB_uvGRWuS-2nSXYwIAqifg/FVcBprEi9WA.jpg?size=564x826&quality=96&sign=f662da003ad11f46060ef09cbead51d9&type=album">
+            </li>
+        </ul>
+        <h2 class="nomera">НОМЕРА</h2>
+        <ul class="room-pictures">
+            <li class="pic">
+                <img class="image"
+                     src="https://sun9-71.userapi.com/impg/iOYu2ZDepv5htoRwVed3lG6nQB1gMO5yF2z8kQ/Asq4XuPhmdE.jpg?size=563x751&quality=95&sign=642212da7e12927870c03cd0ad9acacf&type=album"
+                     alt="Стандарт" width="120" height="120">
+                <h3 class="title">Стандарт</h3>
+            </li>
+            <li class="pic">
+                <img class="image"
+                     src="https://static.overlay-tech.com/assets/24e98252-c63b-49cb-846e-6717d8d9793c.png"
+                     alt="Люкс" width="120" height="120">
+                <h3 class="title">Люкс</h3>
+            </li>
+            <li class="pic">
+                <img class="image"
+                     src="https://sun9-29.userapi.com/impg/H_OfHOAjV6hEs0zVwYRLUNoiOmXIlcUOmzi47A/K7f9u6XeRIc.jpg?size=563x469&quality=95&sign=3e731f2f82175bddb69e58c6945b64f1&type=album"
+                     alt="Семейный" width="120" height="120">
+                <h3 class="title">Семейный</h3>
+            </li>
+        </ul>
 
-    
-    </main>    
-    
-    
-  <footer>
-    <?php require('footer.php'); ?>
-  </footer>
+        <h1 class="otzyvy">ОTЗЫВЫ</h1>
+
+        <div class="comment-nechet">
+            <p class="user-names-ch">Анастасия Ч.</p>
+            <div class="image-stars">
+                <img
+                        alt=""
+                        src="https://static.overlay-tech.com/assets/061e3033-0d6d-45f4-b538-419b4c89c124.png"
+                        width="200" height="30"
+                />
+            </div>
+
+            <p class="strana-prebyvanija-oaje-gorod-dubaj-5-zv">
+                <strong
+                        class="strana-prebyvanija-oaje-gorod-dubaj-5-zv-emphasis-0"
+                >
+                </strong
+                ><strong
+                        class="strana-prebyvanija-oaje-gorod-dubaj-5-zv-emphasis-1"
+                >Страна Пребывания</strong
+                >:
+                <strong
+                        class="strana-prebyvanija-oaje-gorod-dubaj-5-zv-emphasis-3"
+                >ОАЭ<br/> </strong
+                ><strong
+                        class="strana-prebyvanija-oaje-gorod-dubaj-5-zv-emphasis-1"
+                >Город</strong
+                >:
+                <strong
+                        class="strana-prebyvanija-oaje-gorod-dubaj-5-zv-emphasis-3"
+                >Дубай<br/> </strong><strong
+                        class="strana-prebyvanija-oaje-gorod-dubaj-5-zv-emphasis-7">5 звёзд <br/> </strong>
+                Оправданно дорогой качественный отдых.<br/>
+                Отдыхали вновь здесь ради по большей части детей,
+                хотя и нам с мужем здесь очень нравится сервис, еда
+                в разных ресторанах и напитки. Были поражены уровнем
+                и качеством сервиса и бассейном. Мужу очень нравится
+                косметика в номере. Я счастлива, что все довольны
+                отдыхом
+            </p>
+            <img
+                    alt=""
+                    class="spalnja"
+                    src="https://static.overlay-tech.com/assets/24e98252-c63b-49cb-846e-6717d8d9793c.png"
+            />
+        </div>
+
+
+        <div class="comment-chet">
+            <p class="user-names-ch">Вадим К.</p>
+
+            <div class="image-stars">
+                <img
+                        alt=""
+                        class="image-1"
+                        src="https://static.overlay-tech.com/assets/061e3033-0d6d-45f4-b538-419b4c89c124.png"
+                />
+            </div>
+
+            <p class="strana-prebyvanija-rossija-gorod-kazan">
+                <br/>
+                <strong
+                        class="strana-prebyvanija-rossija-gorod-kazan-emphasis-1"
+                >Страна пребывания</strong
+                >:
+                <strong
+                        class="strana-prebyvanija-rossija-gorod-kazan-emphasis-3"
+                >Россия<br/> </strong
+                ><strong
+                        class="strana-prebyvanija-rossija-gorod-kazan-emphasis-1"
+                >Город</strong
+                >:
+                <strong
+                        class="strana-prebyvanija-rossija-gorod-kazan-emphasis-3"
+                >Казань <br/> </strong
+                ><strong
+                        class="strana-prebyvanija-rossija-gorod-kazan-emphasis-7"
+                >5 звёзд<br/> </strong
+                >Отличный отель в сердце города.<br/>
+                Мы были в отеле летом. Чудесная локация. Заезжали
+                ночью- ресепшн очень оперативно заселил. Разнообразный
+                завтрак, который можно съесть на открытой веранде. В
+                номере было очень чисто, окна открываются. Матрац
+                очень комфортный. Рекомендую!
+            </p>
+            <img
+                    alt=""
+                    class="spalnja"
+                    src="https://static.overlay-tech.com/assets/6ae1ec8e-4211-4f6a-bd48-2e57e2f388b8.png"
+            />
+        </div>
+
+        <div class="comment-nechet">
+            <p class="user-names-ch">Катрина С.</p>
+            <div class="image-stars">
+                <img
+                        alt=""
+                        class="image-3"
+                        src="https://static.overlay-tech.com/assets/5dc76f78-c5e8-4649-aa66-4094bad8295a.png"
+                />
+            </div>
+            <p class="strana-prebyvanija-germanija-gorod-mjunhe">
+                <br/>
+                <strong
+                        class="strana-prebyvanija-germanija-gorod-mjunhe-emphasis-1"
+                >Страна Пребывания:</strong
+                >
+                <strong
+                        class="strana-prebyvanija-germanija-gorod-mjunhe-emphasis-3"
+                >Германия<br/> </strong
+                ><strong
+                        class="strana-prebyvanija-germanija-gorod-mjunhe-emphasis-1"
+                >Город:</strong
+                >
+                <strong
+                        class="strana-prebyvanija-germanija-gorod-mjunhe-emphasis-3"
+                >Мюнхен<br/> </strong
+                ><strong
+                        class="strana-prebyvanija-germanija-gorod-mjunhe-emphasis-7"
+                >4.9 звезд<br/> </strong
+                >Проживали с 27.12 по 01.01. Очень неплохой отель,
+                внимательный и дружелюбный персонал. Шикарный завтрак.
+                Расположение отеля просто идеальное. Однако конечно по
+                вечерам шумновато, но это издержки опять таки места. В
+                целом пребывание понравилось, достаточно уютно. Ценник
+                очень приятен.
+            </p>
+            <img
+                    alt=""
+                    class="spalnja"
+                    src="https://static.overlay-tech.com/assets/f055c05b-6188-4d96-a2be-24475328a878.png"
+            />
+        </div>
+
+    </main>
+
+
+    <footer>
+        <div class="main-footer"><p>Контакты</p>
+            <ul class="contacts">
+                <li class="email"><img src="img/letter.svg" width="37" height="26" alt="Иконка письма"><a href="#">Stoyanka@info.ru</a>
+                </li>
+                <li class="phone"><img src="img/phone.png" width="30" height="30" alt="Иконка телефонной трубки"><a
+                            href="tel:+79999999999">+7 (999)999-99-99</a></li>
+            </ul>
+        </div>
+    </footer>
 </body>
 </html> 
